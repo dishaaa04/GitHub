c9019adc3000dad814f29fcbfd7923d349e1b732
dishaaa04 <dishaacharyaa04@gmail.com>
2025-03-08 11:53:03 +0530

    Adding java file

---
+++ Git.java
@@ -0,0 +1,12 @@
+class Git{
+public static void main(String[] args){
+System.out.println("git clone repository Url");
+System.out.println("git add file name");
+System.out.println("git commit -m "description"");
+System.out.println("git push");
+System.out.println("git checkout -b branchname");
+System.out.println("git push -u origin branchname");
+
+}
+}
+
